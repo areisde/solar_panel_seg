da413547f3bf5d099a2061977be6d5cb3134c1ab
Cezary Januszek <cezary.januszek@epfl.ch>
2021-12-16 17:05:30 +0100

    Augmentation

--- packages/helpers.py
+++ packages/helpers.py
@@ -7,54 +7,22 @@ def get_training_augmentation():
     train_transform = [
 
         albu.HorizontalFlip(p=0.5),
-
-        #albu.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0),
-
-        #albu.PadIfNeeded(min_height=320, min_width=320, always_apply=True, border_mode=0),
-        albu.RandomCrop(height=320, width=320, always_apply=True),
-        
+        albu.VerticalFlip(p=0.5),
+        albu.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=0.5, border_mode=0, mask_value=1),
+        albu.PadIfNeeded(min_height=320, min_width=320, always_apply=True, border_mode=0, mask_value=1),
+        #albu.RandomCrop(height=320, width=320, p=1),
         albu.augmentations.transforms.GaussNoise(p=0.2),
-
-        albu.augmentations.geometric.transforms.Perspective(p=0.5),
-
-        albu.RandomBrightness(p=1),
-
-        albu.RandomContrast(p=0.7),
+        albu.augmentations.geometric.transforms.Perspective(p=0.8),
+        albu.RandomBrightness(p=0.5),
+        albu.RandomContrast(p=0.5),
         
-       # albu.OneOf(
-            #[
-              #  albu.CLAHE(p=1),
-                #albu.RandomBrightness(p=1),
-               # albu.RandomGamma(p=1),
-           # ],
-           # p=0.9,
-       # ),
-
-        #albu.OneOf(
-           # [
-               # albu.augmentations.transforms.Sharpen(p=1),
-                #albu.IAASharpen(p=1),
-                #albu.Blur(blur_limit=3, p=1),
-                #albu.MotionBlur(blur_limit=3, p=1),
-           # ],
-           # p=0.9,
-        #),
-
-        #albu.OneOf(
-            #[
-                #albu.augmentations.transforms.RandomBrightness(p=1),
-               # albu.RandomContrast(p=1),
-               # albu.HueSaturationValue(p=1),
-           # ],
-           # p=0.9,
-        #),
     ]
     return albu.Compose(train_transform)
 
 def get_validation_augmentation():
     """Add paddings to make image shape divisible by 32"""
     test_transform = [
-        albu.PadIfNeeded(384, 480)
+        albu.PadIfNeeded(384, 480, border_mode=0, mask_value=1)
     ]
     return albu.Compose(test_transform)
 
